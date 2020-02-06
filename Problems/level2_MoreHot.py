3ec37f8c61bf86819f05e098b27c43d7b8775ac3
kyungminkim-dev <57028248+kyungminkim-dev@users.noreply.github.com>
2020-02-06 13:46:45 +0900

    Add files via upload
    https://programmers.co.kr/learn/courses/30/lessons/42626

---
+++ Problems/level2_MoreHot.py
@@ -0,0 +1,30 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    flag = True
+    for i in scoville:
+        if i >= K:
+            continue
+        else:
+            flag = False
+            break
+    if flag == True:
+        return 0
+    flag2 = True
+    while flag2:
+        if len(scoville) < 2:
+            return -1
+        new = 0
+        new += heapq.heappop(scoville)
+        new += 2 * heapq.heappop(scoville)
+        heapq.heappush(scoville,new)
+        answer += 1
+        for i in scoville:
+            if i >= K:
+                flag2 = False
+                continue
+            else:
+                flag2 = True
+                break
+    return answer
